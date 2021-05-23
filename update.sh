c98f9b795aeade5362e7fcdb48ec3d8d677d370b
Dev_Shadab_Hacker <80148099+hackyshadab@users.noreply.github.com>
2021-05-23 04:23:49 -0400

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,45 @@
+clear
+
+sudo chmod +x /etc/
+
+clear
+
+sudo chmod +x /usr/share/doc
+
+clear
+
+sudo rm -rf /usr/share/doc/hacktronian/
+
+clear
+
+cd /etc/
+
+clear
+
+sudo rm -rf /etc/thehackingsage
+
+clear
+
+mkdir thehackingsage
+
+clear
+
+cd thehackingsage
+
+clear
+
+git clone https://github.com/thehackingsage/hacktronian.git
+
+clear
+
+cd hacktronian
+
+clear
+
+sudo chmod +x install.sh
+
+clear
+
+./install.sh
+
+clear
